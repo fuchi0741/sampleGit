f012ad1b86d1fa55a735ab9e3a779e321869f1ca
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-10-02 18:37:34 +0900

    f

--- sampleGit/ViewController.swift
+++ sampleGit/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("GitFlowのテストです")
+        print("強強エンジニアなので実装スピード早いです")
+        print("完了")
     }
     
     func run() {
